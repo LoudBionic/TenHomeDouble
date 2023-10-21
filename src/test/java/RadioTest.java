d8f462721e53ae2504b3c75d28636b9368b88524
Gleb <Sephirothash@yandex.ru>
2023-10-21 19:20:47 +0300

    NEW

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -2,10 +2,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RadioTest {
+
     @Test
-    public void shouldCreate()
-    {
-        Radio radio = new Radio();
-    }
+    public void should() {
+        Radio rad = new Radio();
+
 
     }
+}
